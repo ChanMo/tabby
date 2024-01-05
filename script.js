dd16db81ec906a645284805e5316ae191f025f14
dyou <chen.orange@aliyun.com>
2024-01-05 14:56:58 +0800

    update js

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-var pending = false; // global variable
-document.querySelectorAll("[contenteditable=true]").forEach(ele => {
-  ele.addEventListener("focus", (e) => {
-    document.addEventListener('keydown', async function(event) {
-      if (ele.textContent.trim() != '' && event.keyCode === 9 && !pending) {
-        event.preventDefault();
-        pending = true;
-        const res = await chrome.runtime.sendMessage({'text':ele.textContent});
-        ele.textContent += res;
-        pending = false;
-      }
-    });
-  })
-})
-document.querySelectorAll("input, textarea").forEach(ele => {
-  ele.addEventListener("focus", (e) => {
-    document.addEventListener('keydown', async function(event) {
-      if (ele.value.trim() != '' && event.keyCode === 9 && !pending) {
-        event.preventDefault();
-        pending = true;
-        const res = await chrome.runtime.sendMessage({'text':ele.value});
-        ele.value += res;
-        pending = false;
-      }
-    });
-  })
-})
+var pending = false; // global
+
+document.addEventListener('keydown', async function(event) {
+  if (event.keyCode === 9 && !pending) {
+    const srcElement = event.srcElement;
+    if(['INPUT', 'TEXTAREA'].indexOf(srcElement.tagName) <= -1 && !srcElement.hasAttribute("contenteditable")) {
+      return;
+    }
+    const isInput = ['INPUT', 'TEXTAREA'].indexOf(srcElement.tagName) > -1;
+    const value = (isInput ? srcElement.value : srcElement.textContent).trim();
+    if(!value) {
+      return;
+    }
+    event.preventDefault();
+    pending = true;
+    if(isInput) {
+      srcElement.value += '...';
+    } else {
+      srcElement.textContent += '...';
+    }
+    const res = await chrome.runtime.sendMessage({'text':value});
+    if(isInput) {
+      srcElement.value = srcElement.value.substring(0, srcElement.value.length - 3) + res;
+    } else {
+      srcElement.textContent = srcElement.textContent.substring(0, srcElement.textContent.length - 3) + res;
+    }
+    pending = false;
+  }
+});
